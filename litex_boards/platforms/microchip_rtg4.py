ac3e3d9df03fa867f13d7c6ad8c43e7ff40c1436
Jijeesh M <jijeesh@localhost.localdomain>
2025-12-15 16:12:10 +0530

    added rtg4 board

---
+++ litex_boards/platforms/microchip_rtg4.py
@@ -0,0 +1,152 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2020 Piotr Esden-Tempski <piotr@esden.net>
+# SPDX-License-Identifier: BSD-2-Clause
+
+# iCEBreaker FPGA:
+# - Crowd Supply campaign: https://www.crowdsupply.com/1bitsquared/icebreaker
+# - 1BitSquared Store: https://1bitsquared.com/products/icebreaker
+# - Design files: https://github.com/icebreaker-fpga/icebreaker
+
+from litex.build.generic_platform import *
+from litex.build.microsemi import MicrosemiPlatform # Assuming creative board uses Microsemi
+#from litex.build.microsemi.programmer import FlashProExpressProgrammer
+
+# IOs ----------------------------------------------------------------------------------------------
+  
+
+_io = [
+
+       ("clk50",     0, Pins("AA39"), IOStandard("LVCMOS25")),
+       # SW1 Switch on board is reset input
+
+
+       # SW1 and SW2 are active high input, SW1 is used as high reset input  for processor
+
+       ("reset_n", 0, Pins("AA30"), IOStandard("LVCMOS25")),
+       ("user_sw2", 0, Pins("AB31"), IOStandard("LVCMOS25")),
+
+
+       #  same as the SW1  & SW@ , only for older projects which uses this name , remove later
+       ("user_btn2", 0, Pins("H12"), IOStandard("LVCMOS25")),
+       ("user_btn1", 0, Pins("H13"), IOStandard("LVCMOS25")),
+
+
+       ("user_led",  0, Pins("W35"), IOStandard("LVCMOS25")),
+       ("user_led",  1, Pins("W34"), IOStandard("LVCMOS25")),
+       ("user_led",  2, Pins("V30"), IOStandard("LVCMOS25")),
+       ("user_led",  3, Pins("W33"), IOStandard("LVCMOS25")),
+       #("user_sw",   0, Pins("H12"), IOStandard("LVCMOS25")),
+       #("user_sw",   1, Pins("H13"), IOStandard("LVCMOS25")),
+       #("user_sw",   2, Pins("M15"),  IOStandard("LVCMOS25")),
+       #("user_sw",   3, Pins("N15"),  IOStandard("LVCMOS25")),
+
+       ("DEVRESET_N" , 0, Pins("M11")),  #
+       
+       ("serial", 0,
+        Subsignal("tx", Pins("E28")),
+        Subsignal("rx", Pins("E27")),
+        IOStandard("LVCMOS25"),
+       ),
+
+
+       ("jtag", 0,
+        Subsignal("tck", Pins("R14")),
+        Subsignal("tms", Pins("N11")),
+        Subsignal("tdo", Pins("P14")),
+        Subsignal("tdi", Pins("P16")),
+        Subsignal("trstb", Pins("N13")),
+        
+    ),
+       
+       
+       ("spiflash", 0,
+        Subsignal("cs_n", Pins("J12")),
+        Subsignal("clk", Pins("J14")),
+        Subsignal("mosi", Pins("K12")),
+        Subsignal("miso", Pins("J13")),
+        IOStandard("LVCMOS33")
+       ),
+       
+       #swapping the mosi and miso pins for testing 
+       ("spiflash_swap", 0,
+        Subsignal("cs_n", Pins("J12")),
+        Subsignal("clk", Pins("J14")),
+        Subsignal("mosi", Pins("J13")),
+        Subsignal("miso", Pins("K12")),
+        IOStandard("LVCMOS33")
+       ),
+    
+        # separate resource for the WP  and Hold pin.
+       ("spiflash_wp_n",   0, Pins("H14"), IOStandard("LVCMOS33")),    
+       ("spiflash_hold_n", 0, Pins("G16"), IOStandard("LVCMOS33")),
+       
+       
+       ("spiflash4x", 0,
+        Subsignal("cs_n", Pins("J12")),
+        Subsignal("clk", Pins("J14")),
+        Subsignal("dq", Pins("K12 J13 H14 G16")),        
+        IOStandard("LVCMOS33")        
+       ),
+       
+       
+       
+       
+
+
+
+       
+       
+       ("axi_aclk_out", 0, Pins(1)),
+       ("axi_arstn_out", 0, Pins(1)),
+       
+       ("axi",0,
+        Subsignal('awvalid', Pins(1)),
+        Subsignal('awready', Pins(1)),
+        Subsignal('awaddr', Pins(32)),
+        Subsignal('awprot', Pins(3)),
+        Subsignal('wvalid', Pins(1)),
+        Subsignal('wready', Pins(1)),
+        Subsignal('wdata', Pins(32)),
+        Subsignal('wstrb', Pins(4)),
+        Subsignal('bvalid', Pins(1)),
+        Subsignal('bready', Pins(1)),
+        Subsignal('bresp', Pins(2)),
+        Subsignal('arvalid', Pins(1)),
+        Subsignal('arready', Pins(1)),
+        Subsignal('araddr', Pins(32)),
+        Subsignal('arprot', Pins(3)),
+        Subsignal('rvalid', Pins(1)),
+        Subsignal('rready', Pins(1)),
+        Subsignal('rresp', Pins(2)),
+        Subsignal('rdata', Pins(32)),
+    ),
+]
+
+# Connectors ---------------------------------------------------------------------------------------
+
+_connectors = [
+    ("PMOD1A", "4   2 47 45  3 48 46 44"),
+    ("PMOD1B", "43 38 34 31 42 36 32 28"),
+    ("PMOD2",  "27 25 21 19 26 23 20 18")
+]
+
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(MicrosemiPlatform):
+    default_clk_name   = "clk50"
+    default_clk_period = 1e9/50e6
+
+    def __init__(self, device="RT4G150_ES-CG1657", toolchain="libero_soc"):
+        
+        MicrosemiPlatform.__init__(self, device, _io, _connectors, toolchain=toolchain)
+
+    def create_programmer(self):
+        """Create a programmer for this platform."""
+        return FlashProExpressProgrammer(device=self.device)
+
+    def do_finalize(self, fragment):
+        MicrosemiPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk50", loose=True), 1e9/50e6)
---
+++ litex_boards/platforms/polarfire_eval.py
@@ -0,0 +1,175 @@
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2018-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+from litex.build.generic_platform import *
+from litex.build.microsemi import MicrosemiPlatform
+#from litex.build.microsemi.programmer import FlashProExpressProgrammer
+
+# IOs ----------------------------------------------------------------------------------------------
+
+   
+    
+
+_io = [
+    # Clk / Rst
+    ("clk50", 0, Pins("E25"), IOStandard("LVCMOS18")),
+    ("reset_n", 0, Pins("K22"), IOStandard("LVCMOS18")),
+
+    # Leds
+    ("user_led", 0, Pins("F22"), IOStandard("LVCMOS18")),
+    ("user_led", 1, Pins("B26"), IOStandard("LVCMOS18")),
+    ("user_led", 2, Pins("C26"), IOStandard("LVCMOS18")),
+    ("user_led", 3, Pins("D25"), IOStandard("LVCMOS18")),
+    ("user_led", 4, Pins("C27"), IOStandard("LVCMOS18")),
+    ("user_led", 5, Pins("F23"), IOStandard("LVCMOS18")),
+    ("user_led", 6, Pins("H22"), IOStandard("LVCMOS18")),
+    ("user_led", 7, Pins("H21"), IOStandard("LVCMOS18")),
+       
+
+    # Buttons
+    ("user_btn", 0, Pins("H23"), IOStandard("LVCMOS18")),
+    ("user_btn", 1, Pins("D21"), IOStandard("LVCMOS18")),
+    ("user_btn", 2, Pins("H24"), IOStandard("LVCMOS18")),
+    ("user_btn", 3, Pins("C22"), IOStandard("LVCMOS18")),
+
+    # Serial
+    ("serial", 0,
+        Subsignal("tx", Pins("G17")),
+        Subsignal("rx", Pins("H18")),
+        IOStandard("LVCMOS18")
+    ),
+
+     # SPIFlash
+    ("spiflash", 0,
+        Subsignal("clk",  Pins("H19")),
+        Subsignal("cs_n", Pins("A20")),
+        Subsignal("mosi", Pins("G19")),
+        Subsignal("miso", Pins("C18")),
+        #Subsignal("wp",   Pins("A27")),  
+        #Subsignal("hold", Pins("A22")), 
+        IOStandard("LVCMOS18")
+    ),
+
+    # 2. A new, separate resource for the Write Protect pin.
+    #    The "_n" suffix is a convention for active-low signals.
+    ("spiflash_wp_n", 0, Pins("A27"), IOStandard("LVCMOS18")),
+
+    # 3. A new, separate resource for the Hold pin.
+    ("spiflash_hold_n", 0, Pins("A22"), IOStandard("LVCMOS18")),
+    
+    ("spiflash4x", 0,
+        Subsignal("clk",  Pins("H19")),
+        Subsignal("cs_n", Pins("A20")),
+        Subsignal("dq",   Pins("G19 C18 A27 A22")),
+        IOStandard("LVCMOS18")
+     
+    ),
+
+    # This is he PF_SPI ports 
+    
+    ("spiflash_dedicated", 0,
+        Subsignal("clk",  Pins("G3")),
+        Subsignal("cs_n", Pins("G4")),
+        Subsignal("mosi", Pins("G5")),
+        Subsignal("miso", Pins("G10")),
+        # These connect to the FLASH and IFACE ports of the macro
+        Subsignal("flash", Pins("H10")),
+        Subsignal("iface", Pins("G9")),
+        IOStandard("LVCMOS25") # Or the correct I/O standard
+    ),
+
+
+    ##### Below ports are not tested, needs to verify.
+    
+        
+    # Fabric JTAG
+    ("jtag", 0,
+        Subsignal("tck",  Pins("KF8")),
+        Subsignal("tms",  Pins("F7")),
+        Subsignal("tdo",  Pins("F6")),
+        Subsignal("tdi",  Pins("G8")),
+        Subsignal("trstb", Pins("G7")),
+        IOStandard("LVCMOS25")
+    ),   
+    
+    
+    
+
+    # Ethernet
+    ("eth_clocks", 0,
+        Subsignal("tx", Pins("J8")),
+        Subsignal("rx", Pins("K3")),
+        IOStandard("LVCMOS18")
+    ),
+    ("eth", 0,
+        Subsignal("rst_n",   Pins("L8"), IOStandard("LVCMOS33")),
+        Subsignal("int_n",   Pins("J4")),
+        Subsignal("mdio",    Pins("H2")),
+        Subsignal("mdc",     Pins("J2")),
+        Subsignal("rx_ctl",  Pins("K5")),
+        Subsignal("rx_data", Pins("J9 K1 K6 K4")),
+        Subsignal("tx_ctl",  Pins("L5")),
+        Subsignal("tx_data", Pins("K8 L1 L2 L3")),
+        IOStandard("LVCMOS18")
+    ),
+
+    # --- Corrected DDR3 Definitions ---
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            "AL27 AL26 AM27 AN27 AN26 AP25 AL25 AK25 AJ23 AH23 "
+            "AJ25 AJ24 AL22 AK23 AL24 AL23"
+        )),
+        Subsignal("ba",      Pins("AE25 AD23 AD25")),
+        Subsignal("cas_n",   Pins("AF25")),
+        Subsignal("clk_p",   Pins("AP26")),
+        Subsignal("clk_n",   Pins("AP27")),
+        Subsignal("cke",     Pins("AF22")),
+        Subsignal("cs_n",    Pins("AE22")),
+        Subsignal("dm",      Pins("AN23 AL20")),
+        Subsignal("dq", Pins(
+            "AN22 AN21 AM24 AN24 AP21 AP20 AP19 AN19 AM21 AK22 "
+            "AK21 AK20 AJ20 AH21 AG21 AM19"
+        )),
+        Subsignal("dqs_p",   Pins("AP23 AH22")),
+        Subsignal("dqs_n",   Pins("AP24 AJ21")),
+        Subsignal("odt",     Pins("AF23")),
+        Subsignal("ras_n",   Pins("AE23")),
+        Subsignal("reset_n", Pins("AG22")),
+        Subsignal("we_n",    Pins("AF24")),
+        IOStandard("SSTL15II"),
+    ),
+
+    ("ddram_shield", 0,
+        Pins("AM22 AM20"),
+        IOStandard("SHIELD15"),
+    ),
+    
+    
+
+    
+
+]
+
+
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(MicrosemiPlatform):
+    default_clk_name   = "clk50"
+    default_clk_period = 1e9/50e6
+
+    def __init__(self, device="MPF300T-1FCG1152I", toolchain="libero_soc"):
+        MicrosemiPlatform.__init__(self, device, _io, toolchain=toolchain)
+        
+    def create_programmer(self):
+        """Create a programmer for this platform."""
+        return FlashProExpressProgrammer(device=self.device)
+
+    def do_finalize(self, fragment):
+        MicrosemiPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("clk50", 0, loose=True), 1e9/50e6)
+        self.add_period_constraint(self.lookup_request("clk50", 1, loose=True), 1e9/50e6)
+        
